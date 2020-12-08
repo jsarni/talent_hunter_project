ac61079f8d49599f358f591de148e0f3048ba8d9
jsarni <45796640+jsarni@users.noreply.github.com>
2020-12-08 16:13:22 +0100

    added oozie scripts for spotify collect app (#6)

---
+++ collect/script/oozie/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Get property from properties file
+
+getProperty(){
+  grep "^$2=" "$1" | cut -d'=' -f2
+}
+
+# Get current dir
+DIR = "$(pwd)"
+
+PROPERTIES_FILE = "$DIR/../properties/collect.properties"
+
+OOZIE_CLIENT = $(getProperty "$PROPERTIES_FILE" "oozie_client")
+OOZIE="http://$OOZIE_OOZIE_CLIENT/oozie"
+OOZIE_COORD_NAME = $(getProperty "$PROPERTIES_FILE" "oozie_coordinator_name")
+
+START_DATE = $(getProperty "$PROPERTIES_FILE" "start_date")
+END_DATE = $(getProperty "$PROPERTIES_FILE" "end_date")
+
+## Kill previous coordinator
+oozie jobs --oozie ${OOZIE} -jobtype coordinator -filter name=${OOZIE_COORD_NAME} -kill
+
+## Run coordinator
+
+oozie job --oozie ${OOZIE} -config ${PROPERTIES_FILE} -run start_date=$START_DATE
